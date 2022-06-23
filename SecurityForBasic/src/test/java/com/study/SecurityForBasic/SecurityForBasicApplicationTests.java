cdc954cf8ede552f6376f1fd947e366e21028bae
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-06-23 19:00:15 +0900

    initial commit

---
+++ SecurityForBasic/src/test/java/com/study/SecurityForBasic/SecurityForBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.SecurityForBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityForBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
