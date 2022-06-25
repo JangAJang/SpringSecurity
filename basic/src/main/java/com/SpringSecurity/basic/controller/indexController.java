dce3eb357c7984a3e6a8ebd4f5a4d624c6c50c16
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-06-25 11:03:49 +0900

    initial commit

--- basic/src/main/java/com/SpringSecurity/basic/controller/indexController.java
+++ basic/src/main/java/com/SpringSecurity/basic/controller/indexController.java
@@ -3,6 +3,7 @@ package com.SpringSecurity.basic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class indexController {
@@ -10,5 +11,36 @@ public class indexController {
     @GetMapping({"", "/"})
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/joinProc")
+    @ResponseBody
+    public String joinProc(){
+        return "회원가입 완료";
+    }
+
+    @GetMapping("/join")
+    public String join(){
+        return "join";
     }
 }
