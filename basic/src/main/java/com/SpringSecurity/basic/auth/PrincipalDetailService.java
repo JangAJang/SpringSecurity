d531bdb6b3dc66e001561ebaf6be77a46770e70b
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-06-30 19:47:29 +0900

    initial commit

--- basic/src/main/java/com/SpringSecurity/basic/auth/PrincipalDetailService.java
+++ basic/src/main/java/com/SpringSecurity/basic/auth/PrincipalDetailService.java
@@ -1,5 +1,8 @@
 package com.SpringSecurity.basic.auth;
 
+import com.SpringSecurity.basic.model.User;
+import com.SpringSecurity.basic.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,37 +10,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PrincipalDetailService implements UserDetailsService {
-    public PrincipalDetailService() {
-        super();
-    }
 
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User userEntity = userRepository.findByUsername(username);
+        if(userEntity != null) return new PrincipalDetails(userEntity);
+        else return null;
     }
 }
