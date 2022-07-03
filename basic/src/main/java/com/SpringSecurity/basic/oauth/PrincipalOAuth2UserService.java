3c0e0fec7b5058300a8afe6ada3705baf873ce0b
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-07-03 14:38:07 +0900

    initial commit

--- basic/src/main/java/com/SpringSecurity/basic/oauth/PrincipalOAuth2UserService.java
+++ basic/src/main/java/com/SpringSecurity/basic/oauth/PrincipalOAuth2UserService.java
@@ -1,5 +1,7 @@
 package com.SpringSecurity.basic.oauth;
 
+import com.SpringSecurity.basic.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -8,8 +10,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
+
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        
+
         return super.loadUser(userRequest);
+
     }
+
+
 }
